51cf2ecd45401464fcf77b91162fb904ab8ef877
Nathan-Yennupiak <nathanyennupiak1@gmail.com>
2024-12-12 12:00:32 +0000

    Blog section and card design

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,9 +3,9 @@ import Link from "next/link"
 
 const Hero = () => {
     return (
-        <div className="lg:px-40 px-4 py-8 flex flex-col md:flex-row gap-10 lg:gap-2">
-            <div className="flex flex-col space-y-4 justify-end flex-1 text-center md:text-left">
-                <h1 className="capitalize text-3xl md:text-5xl font-bold ">A blog for all ages and website lovers</h1>
+        <div className="lg:px-40 px-4 py-20 flex flex-col md:flex-row gap-10 lg:gap-2 bg-primary-gray mb-10">
+            <div className="flex flex-col space-y-4 justify-center flex-1 text-center md:text-left">
+                <h1 className="capitalize text-5xl lg:text-7xl font-bold ">A blog for all ages & website lovers</h1>
                 <p>This Blog is perfect if you have a blog idea and you want to share. Using its high-quality sections and components you can showcase your blog articles in a very beautiful and unique way.</p>
                 <Link href="/contact" className="bg-primary-brand hover:bg-primary-dark flex justify-center text-primary-bg w-44 py-2 rounded mx-auto md:mx-0">Contact Us</Link>
             </div>
